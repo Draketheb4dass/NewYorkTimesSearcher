68c0429f661758edb0f2a6a01d8e816cc101aae3
Jephte Drake Colin <drakecolinj@gmail.com>
2018-07-11 06:40:50 -0400

    Click listener implemented along with Intent
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/newyorktimessearcher/activities/ArticleActivity.java
+++ app/src/main/java/com/realty/drake/newyorktimessearcher/activities/ArticleActivity.java
@@ -3,6 +3,8 @@ package com.realty.drake.newyorktimessearcher.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.realty.drake.newyorktimessearcher.R;
+
 public class ArticleActivity extends AppCompatActivity {
 
     @Override
