b6921568373db17d81ad2e5459047e3769d29290
Jephte Drake Colin <drakecolinj@gmail.com>
2018-07-11 12:40:41 -0400

    custom Toolbar added
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/newyorktimessearcher/activities/ArticleActivity.java
+++ app/src/main/java/com/realty/drake/newyorktimessearcher/activities/ArticleActivity.java
@@ -2,19 +2,27 @@ package com.realty.drake.newyorktimessearcher.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebResourceRequest;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.realty.drake.newyorktimessearcher.Article;
 import com.realty.drake.newyorktimessearcher.R;
 
-public class ArticleActivity extends AppCompatActivity {
+public class ArticleActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
+        //Set custom AppBar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.article_toolbar);
+        myToolbar.inflateMenu(R.menu.menu_detail);
+        setSupportActionBar(myToolbar);
+        myToolbar.setOnMenuItemClickListener(this);
 
         Article article = getIntent().getParcelableExtra("article");
         WebView webView =(WebView) findViewById(R.id.wvArticle);
@@ -27,5 +35,22 @@ public class ArticleActivity extends AppCompatActivity {
             }
         });
         webView.loadUrl(article.getWebUrl());
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                Toast.makeText(this, "Shared", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
     }
 }
