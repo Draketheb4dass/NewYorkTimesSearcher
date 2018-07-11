67109fd5b084a517a0e8c8ac1e0a637d444d5914
Jephte Drake Colin <drakecolinj@gmail.com>
2018-07-11 04:29:17 -0400

    OnArticleSearch method implemented
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/newyorktimessearcher/SearchActivity.java
+++ app/src/main/java/com/realty/drake/newyorktimessearcher/SearchActivity.java
@@ -5,16 +5,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 public class SearchActivity extends AppCompatActivity {
     EditText etQuery;
     GridView gvResults;
     Button btnSearch;
+    String APIKey= "c984070a9b894daf976427235eb46ea5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +36,7 @@ public class SearchActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setupViews();
 
-        String APIKey= "c984070a9b894daf976427235eb46ea5";
+
 
 
 
@@ -38,7 +50,32 @@ public class SearchActivity extends AppCompatActivity {
 
     public void onArticleSearch(View view) {
         String query = etQuery.getText().toString();
-        Toast.makeText(this, "Searching for " + query,
-                Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Searching for " + query,
+        //        Toast.LENGTH_SHORT).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String SEARCH_API_URL =
+                "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        RequestParams params = new RequestParams();
+        params.put("api-key", APIKey);
+        params.put("page", 0);
+        params.put("q", query);
+
+        client.get(SEARCH_API_URL, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+                JSONArray articleJsonResults = null;
+
+                try {
+                    articleJsonResults = response
+                            .getJSONObject("response")
+                            .getJSONArray("docs");
+                }catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
 }
