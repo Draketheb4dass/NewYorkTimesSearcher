9aca08311144a4c9b4db7411f5d2c9899c4c2e15
Jephte Drake Colin <drakecolinj@gmail.com>
2018-07-11 12:58:57 -0400

    Share button implemented
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/newyorktimessearcher/activities/ArticleActivity.java
+++ app/src/main/java/com/realty/drake/newyorktimessearcher/activities/ArticleActivity.java
@@ -1,5 +1,6 @@
 package com.realty.drake.newyorktimessearcher.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -39,9 +40,15 @@ public class ArticleActivity extends AppCompatActivity implements Toolbar.OnMenu
 
     @Override
     public boolean onMenuItemClick(MenuItem item){
+        Article article = getIntent().getParcelableExtra("article");
         switch (item.getItemId()) {
+            //Share link Intent
             case R.id.action_share:
-                Toast.makeText(this, "Shared", Toast.LENGTH_SHORT).show();
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Check this out");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, article.getWebUrl());
+                startActivity(shareIntent.createChooser(shareIntent, article.getWebUrl()));
                 return true;
         }
         return true;
