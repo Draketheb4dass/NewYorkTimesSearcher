def58e97cbe137e547475d843ba3a22166ddb442
Jephte Drake Colin <drakecolinj@gmail.com>
2018-07-11 06:20:30 -0400

    Implement getView in ArticleArrayAdapter
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/newyorktimessearcher/adapters/ArticleArrayAdapter.java
+++ app/src/main/java/com/realty/drake/newyorktimessearcher/adapters/ArticleArrayAdapter.java
@@ -1,9 +1,18 @@
 package com.realty.drake.newyorktimessearcher.adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.realty.drake.newyorktimessearcher.Article;
+import com.realty.drake.newyorktimessearcher.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,5 +21,36 @@ public class ArticleArrayAdapter extends ArrayAdapter<Article> {
 
     public ArticleArrayAdapter(Context context, List<Article> articles) {
         super(context, android.R.layout.simple_list_item_1, articles);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        Article article = this.getItem(position);
+
+        //Check if an existing view is being reuse, otherwise inflate the view
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.item_article_result, parent, false);
+        }
+        //find Image View
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.ivImage);
+
+        //clear out recycled image from last time
+        imageView.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTile);
+        assert article != null;
+        tvTitle.setText(article.getHeadline());
+
+        //populate the thumbnail image
+        //remote download the image in the background
+        String thumbnail = article.getThumbNail();
+        if (!TextUtils.isEmpty(thumbnail)){
+            Picasso.with(getContext()).load(thumbnail).into(imageView);
+        }
+        return convertView;
+
+
     }
 }
