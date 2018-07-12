1bc6dc68ace40c07972ccc64fc716d6e454eee42
Jephte Drake Colin <drakecolinj@gmail.com>
2018-07-12 04:37:19 -0400

    FilterDialogFragment created
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

---
+++ app/src/main/java/com/realty/drake/newyorktimessearcher/fragments/FilterDialogFragment.java
@@ -0,0 +1,17 @@
+package com.realty.drake.newyorktimessearcher.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.realty.drake.newyorktimessearcher.R;
+
+
+public class FilterDialogFragment extends DialogFragment {
+
+}
