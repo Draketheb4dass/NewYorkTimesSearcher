2bac25bbc66506537fdf11587c95d716db3be694
Jephte Drake Colin <drakecolinj@gmail.com>
2018-07-12 16:15:04 -0400

    FilterDialogFragment button click listener set
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/realty/drake/newyorktimessearcher/fragments/FilterDialogFragment.java
+++ app/src/main/java/com/realty/drake/newyorktimessearcher/fragments/FilterDialogFragment.java
@@ -1,32 +1,45 @@
 package com.realty.drake.newyorktimessearcher.fragments;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.realty.drake.newyorktimessearcher.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
 
 
-public class FilterDialogFragment extends DialogFragment {
+public class FilterDialogFragment extends DialogFragment  {
+    private Button mBtnSubmitFilt;
 
     public FilterDialogFragment() {} //Empty Constructor is required for DialogFragment
 
-    public static FilterDialogFragment newInstance(String title) {
 
+    //Defines the listener interface with a method passing back data result.
+    public interface FilterDialogListener{
+        void onFinishFilterDialog(HashMap filter);
+    }
+
+    public static FilterDialogFragment newInstance(String title) {
         FilterDialogFragment frag = new FilterDialogFragment();
         Bundle args = new Bundle();
         args.putString("title", title);
@@ -49,6 +62,19 @@ public class FilterDialogFragment extends DialogFragment {
         assert getArguments() != null;
         String title = getArguments().getString("title", "Enter Name");
         getDialog().setTitle(title);
+
+        //Send data back to fragment
+        mBtnSubmitFilt = (Button) view.findViewById(R.id.button);
+        mBtnSubmitFilt.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dismiss();
+                    }
+                }
+        );
+
+
     }
 
     @Override
@@ -59,6 +85,8 @@ public class FilterDialogFragment extends DialogFragment {
         int height = getResources().getDimensionPixelSize(R.dimen.popup_height);
         getDialog().getWindow().setLayout(width, height);
     }
+
+
 
     // Calendar calendar = Calendar.getInstance();
    // EditText etDate = (EditText) View.findViewById(R.id.etDate);
